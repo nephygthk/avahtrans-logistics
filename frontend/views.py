aa01136b29f8a3d6c8ba762808c9b6cda43759e0
NephygTdkingz <tdkingzict@gmail.com>
2023-05-23 18:37:23 +0100

    added tracking and tracking page

--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
+
+from shipment.models import Shipment
 
 class HomeView(TemplateView):
     template_name = 'frontend/index.html'
@@ -13,5 +16,12 @@ class ContactView(TemplateView):
 
 def TrackingView(request):
     if request.method == 'POST':
-        pass
-    return render(request, 'frontend/tracking.html', {}) 
+        tracking_code = request.POST.get('tracking_code')
+        shipments = Shipment.objects.filter(tracking_number=tracking_code)
+        if shipments.exists():
+            return render(request, 'frontend/tracking.html', {'shipments':shipments}) 
+        else:
+            messages.error(request, "Invalid tracking code. Please check the code and try again. please make sure no spaces in between the letters")
+            return redirect('frontend:home')
+    return redirect('frontend:home')
+    # return render(request, 'frontend/tracking.html') 
